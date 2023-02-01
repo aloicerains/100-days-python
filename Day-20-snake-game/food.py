4e5dc598a5e0f4cc6c8dbeeca4b60e0981876b1c
aloicerains <aloiceokoth4@gmail.com>
2023-02-01 20:43:58 +0300

    food module

---
+++ Day-20-snake-game/food.py
@@ -0,0 +1,22 @@
+"""
+Food Module
+"""
+import random
+from turtle import Turtle
+
+
+class Food(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape('circle')
+        self.penup()
+        self.shapesize(stretch_wid=0.5, stretch_len=0.5)
+        self.color('blue')
+        self.speed("fastest")
+        self.refresh()
+
+    def refresh(self):
+        rand_x = random.randint(-280, 280)
+        rand_y = random.randint(-280, 280)
+        self.goto(rand_x, rand_y)
+
